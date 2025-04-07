363353a904ce04ab549e76bd87f020de81fd3477
jordanlenhart <jordinholenhart@gmail.com>
2025-04-07 16:59:54 +0000

    A for loop allows for us to iterate through variosu indexes of the array and check a condition that evaluates each index. For instance, in this exercise, i use avatarElements as an array with variosu elements. avatarElement starts at index 0 being "Water," and is iterated using avatarElement++ that adds 1 to the index each iteration. The condition that is checked for in the for loop is if avatarElement is less than the length of the avatarElements array; if it is still less than the array, then it will continue iterations. As soon as the condition is not met, the for loop will end. I placed an if statement in the for loop that allows for different statements to be read based on the conditions met.

--- script.js
+++ script.js
@@ -17,9 +17,21 @@ console.log(groceryList.length);
 // 2nd Exercise
 let shoppingCart = [];
 
-shoppingCart.push("Butter", "Jelly", "Pasta")
+shoppingCart.push("Butter", "Jelly", "Pasta");
 console.log(shoppingCart);
 
 let removedItem = shoppingCart.pop();
-console.log(removedItem)
-console.log(shoppingCart)
+console.log(removedItem);
+console.log(shoppingCart);
+
+// 3rd Exercise
+let avatarElements = ["Water", "Earth", "Fire", "Air", "Energy"];
+
+for (let avatarElement = 0; avatarElement < avatarElements.length; avatarElement++) {
+    console.log("Does the Avatar know " + avatarElements[avatarElement] + "?")
+    if (avatarElement < 4) {
+    console.log("The Avatar can bend all elements, including " + avatarElements[avatarElement]);
+    } else {
+        console.log("The Avatar cannot yet bend the element " + avatarElements[avatarElement])
+    }
+}
