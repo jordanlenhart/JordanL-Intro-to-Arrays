ee0db55b695087673cda7affdaa56a9e88d01f4c
jordanlenhart <jordinholenhart@gmail.com>
2025-04-07 16:02:50 +0000

    Arrays are zero indexed, meaning that the first element of the array has an index of 0. This is simply a way of numbering the items. For instance, in this exercise, our first array has a length of 5, but after adding 2 more elements via .length, the list length increases to 7.

---
+++ script.js
@@ -0,0 +1,14 @@
+let groceryList = [];
+
+groceryList[0] = "Milk";
+groceryList[1] = null;
+groceryList[2] = "Veggies";
+groceryList[3] = null;
+groceryList[4] = "Bread";
+
+console.log(groceryList.length);
+
+groceryList[groceryList.length] = "Fruits";
+groceryList[groceryList.length] = "Beef";
+
+console.log(groceryList.length);
