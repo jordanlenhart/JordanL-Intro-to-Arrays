d595299435d5447d9573bc8dad2d0f537a2e84c1
Jordan <jordinholenhart@gmail.com>
2025-04-07 19:14:26 -0700

    Fixed the 1st exercise, the questions are worded very confusingly. All steps complete

--- script.js
+++ script.js
@@ -2,17 +2,23 @@
 let groceryList = [];
 
 groceryList[0] = "Milk";
-groceryList[1] = null;
+groceryList[1] = "Cheese";
 groceryList[2] = "Veggies";
-groceryList[3] = null;
+groceryList[3] = "Eggs";
 groceryList[4] = "Bread";
 
 console.log(groceryList.length);
 
-groceryList[groceryList.length] = "Fruits";
-groceryList[groceryList.length] = "Beef";
+groceryList[1] = null;
+groceryList[3] = null;
+
+groceryList.length = 7;
+
+groceryList[5] = "Fruits";
+groceryList[6] = "Beef";
 
 console.log(groceryList.length);
+console.log(groceryList);
 
 // 2nd Exercise
 let shoppingCart = [];
